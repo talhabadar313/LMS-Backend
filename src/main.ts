7e74b8e04994d77d9c1994410ebe4c189ae51039
talhabadar313 <109822601+talhabadar313@users.noreply.github.com>
2024-09-09 13:09:39 +0500

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://lms-5gm4nilnf-talhas-projects-cc754da5.vercel.app'], 
+    origin: ['https://lms-5gm4nilnf-talhas-projects-cc754da5.vercel.app', 'http://localhost:3001'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, 
