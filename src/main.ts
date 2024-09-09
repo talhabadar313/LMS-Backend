d71517581875870de0f70223cf34069819cfee99
talhabadar313 <109822601+talhabadar313@users.noreply.github.com>
2024-09-09 19:48:02 +0500

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
+// Create an Express server instance
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3001'], 
+    origin: ['https://lms-alpha-five.vercel.app', 'http://localhost:3001'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, 
+    credentials: true,
   });
-  
-  await app.listen(3000);
+
+  await app.init(); // This is important for Vercel to handle requests properly
 }
+
+// Initialize the NestJS application on serverless function execution
 bootstrap();
+
+// Export the express server for Vercel
+export const handler = server;
