dd2e0c5a594ad7d49a5a5b7258352e412e2469c0
talhabadar313 <109822601+talhabadar313@users.noreply.github.com>
2024-09-16 16:03:12 +0500

    minor fix

--- src/batch/batch.resolver.ts
+++ src/batch/batch.resolver.ts
@@ -15,7 +15,7 @@ export class BatchResolver {
 
   @Mutation(() => Batch)
   @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
+  @Roles('admin')
   createBatch(@Args('createBatchInput') createBatchInput: CreateBatchInput) {
     return this.batchService.create(createBatchInput);
   }
@@ -40,17 +40,11 @@ export class BatchResolver {
     return this.batchService.removeTeacherFromBatch(batchId, teacherId);
   }
 
-  @Mutation(() => Batch)
-  @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
   @Query(() => [Batch], { name: 'batches' })
   findAll() {
     return this.batchService.findAll();
   }
 
-  @Mutation(() => Batch)
-  @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
   @Query(() => Batch, { name: 'batch' })
   findOne(@Args('id', { type: () => String }) id: string) {
     return this.batchService.findOne(id);
@@ -73,7 +67,7 @@ export class BatchResolver {
     return this.batchService.remove(id);
   }
 
-  @Mutation(() => Batch)
+  @Query(() => ApplicationsResponse)
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('admin', 'teacher')
   getApplicationsPerDay(@Args('id', { type: () => String }) batchId: string) {
