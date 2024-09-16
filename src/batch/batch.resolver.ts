5e77f6654df1cb366d43f65849375b17b4133751
talhabadar313 <109822601+talhabadar313@users.noreply.github.com>
2024-09-16 15:07:59 +0500

    minor change

--- src/batch/batch.resolver.ts
+++ src/batch/batch.resolver.ts
@@ -15,7 +15,7 @@ export class BatchResolver {
 
   @Mutation(() => Batch)
   @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
+  @Roles('admin')
   createBatch(@Args('createBatchInput') createBatchInput: CreateBatchInput) {
     return this.batchService.create(createBatchInput);
   }
@@ -40,15 +40,11 @@ export class BatchResolver {
     return this.batchService.removeTeacherFromBatch(batchId, teacherId);
   }
 
-  @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
   @Query(() => [Batch], { name: 'batches' })
   findAll() {
     return this.batchService.findAll();
   }
 
-  @UseGuards(AuthGuard, RolesGuard)
-  @Roles('admin', 'teacher')
   @Query(() => Batch, { name: 'batch' })
   findOne(@Args('id', { type: () => String }) id: string) {
     return this.batchService.findOne(id);
