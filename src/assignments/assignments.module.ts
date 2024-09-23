d13a5a85d9b792a496d67ab5c4a5834ef66629b8
talhabadar313 <109822601+talhabadar313@users.noreply.github.com>
2024-09-23 16:06:14 +0500

    minor change

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Assignment } from './entities/assignment.entity';
 import { BatchModule } from '../batch/batch.module';
 import { TopicsModule } from '../topics/topics.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assignment]), BatchModule, TopicsModule],
+  imports: [
+    TypeOrmModule.forFeature([Assignment]),
+    BatchModule,
+    TopicsModule,
+    AuthModule,
+  ],
   providers: [AssignmentsResolver, AssignmentsService],
 })
 export class AssignmentsModule {}
